e426eaa63660a2182a5b0816638f911013476615
Arcanum-64 <superblah64@gmail.com>
2018-04-30 11:07:10 +0100

    Bridge Integration
    Compile errors when bridge sketch was integrated

--- LockBoxTrigger.ino
+++ LockBoxTrigger.ino
@@ -4,6 +4,11 @@
 // based upon:
 // PIR sensor tester by Limor Fried of Adafruit
 // tone code by michael@thegrebs.com
+#include <Bridge.h>
+#include <BridgeServer.h>
+#include <BridgeClient.h>
+
+BridgeServer server;
 
  
 int ledPin = 13;                // choose the pin for the LED
@@ -14,15 +19,147 @@ int pinSpeaker = 10;           //Set up a speaker on a PWM pin (digital 9, 10, o
 
 void setup() {
   pinMode(ledPin, OUTPUT);      // declare LED as output
-  pinMode(inputPin, INPUT);     // declare sensor as input
+  pinMode(inputPin, INPUT);    // declare sensor as input
   pinMode(pinSpeaker, OUTPUT);
   Serial.begin(9600);
+  Bridge.begin();
+
+  server.listenOnLocalhost();
+  server.begin();
 }
 
 void loop(){
-  val = digitalRead(inputPin);  // read input value
+  BridgeClient client = server.accept();
+
+    if (client) {
+    process(client);
+
+    client.stop();
+
+     delay(50); // Poll every 50ms
+}
+  }
+
+  void process(BridgeClient client) {
+  // read the command
+  String command = client.readStringUntil('/');
+
+  // is "digital" command?
+  if (command == "digital") {
+    digitalCommand(client);
+  }
+
+  // is "mode" command?
+  if (command == "mode") {
+    modeCommand(client);
+  }
+}
+
+void digitalCommand(BridgeClient client) {
+  int pin, value;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is a '/' it means we have an URL
+  // with a value like: "/digital/13/1"
+  if (client.read() == '/') {
+    value = client.parseInt();
+    digitalWrite(pin, value);
+  } else {
+    value = digitalRead(pin);
+  }
+
+  // Send feedback to client
+  client.print(F("Pin D"));
+  client.print(pin);
+  client.print(F(" set to "));
+  client.println(value);
+
+    String key = "D";
+  key += pin;
+  Bridge.put(key, String(value));
+}
+void analogCommand(BridgeClient client) {
+  int pin, value;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is a '/' it means we have an URL
+  // with a value like: "/analog/5/120"
+  if (client.read() == '/') {
+    // Read value and execute command
+    value = client.parseInt();
+    analogWrite(pin, value);
+
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" set to analog "));
+    client.println(value);
+
+        // Update datastore key with the current pin value
+    String key = "D";
+    key += pin;
+    Bridge.put(key, String(value));
+  } else {
+    // Read analog pin
+    value = analogRead(pin);
+
+    // Send feedback to client
+    client.print(F("Pin A"));
+    client.print(pin);
+    client.print(F(" reads analog "));
+    client.println(value);
+
+    // Update datastore key with the current pin value
+    String key = "A";
+    key += pin;
+    Bridge.put(key, String(value));
+  }
+}
+
+void modeCommand(BridgeClient client) {
+  int pin;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is not a '/' we have a malformed URL
+  if (client.read() != '/') {
+    client.println(F("error"));
+    return;
+  }
+
+  String mode = client.readStringUntil('\r');
+
+  if (mode == "input") {
+    pinMode(pin, INPUT);
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" configured as INPUT!"));
+    return;
+  }
+
+  if (mode == "output") {
+    pinMode(pin, OUTPUT);
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" configured as OUTPUT!"));
+    return;
+  }
+
+  client.print(F("error: invalid mode "));
+  client.print(mode);
+}
+
+ //sensor 
+
+  pirState = digitalRead(inputPin);  // read input value
   if (val == HIGH) {            // check if the input is HIGH
-    digitalWrite(ledPin, HIGH);  // turn LED ON
     playTone(300, 160);
     delay(150);
 
@@ -34,7 +171,6 @@ void loop(){
       pirState = HIGH;
     }
   } else {
-      digitalWrite(ledPin, LOW); // turn LED OFF
       playTone(0, 0);   
       if (pirState == HIGH){
       // we have just turned of
